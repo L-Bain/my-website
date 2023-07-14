76ef59a9cd0cd54167f4061b7757348c3cd27439
lazsnail <larrybain01@gmail.com>
2023-07-14 15:28:49 +1000

    prettier code

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ let screen_width = document.body.clientWidth;
 let screen_height = document.body.clientHeight;
 
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 // Copy email to clipboard function
 async function copyEmail() {
-    navigator.clipboard.writeText("lazsnail@gmail.com");
-    tooltip.classList.add("show");
-    await sleep(4000);
-    tooltip.classList.remove("show");
+  navigator.clipboard.writeText("lazsnail@gmail.com");
+  tooltip.classList.add("show");
+  await sleep(4000);
+  tooltip.classList.remove("show");
 }
